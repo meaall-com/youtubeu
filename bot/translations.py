a2944fa4ab511091d39160d955cb05c5b0543d05
meaall-com <67676419+meaall-com@users.noreply.github.com>
2020-11-28 15:12:38 +0300

    Add files via upload

---
+++ bot/translations.py
@@ -0,0 +1,38 @@
+
+class Messages:
+
+    START_MSG = "يا سيدي{}.\n\nI' وررررحب يا سيدي الغالي انا بوت رفع الفيديوهات لليوتيوب /help."
+
+    HELP_MSG = [
+        ".",
+        "اضغط تخطي",
+
+        "زيد واحدة",
+
+        "زيد حبة",
+
+        "**Verify your YouTube account**\n\nYoutube take spam and abuse very seriously. So you are asked to verify your Youtube account. Once you've verified your account, you will be able to upload videos longer than 15 minutes. If you haven't verified your account every video uploaded which are longer than 15 minutes will be removed.\n[Verify your Youtube account here.](http://www.youtube.com/verify)\n\n__Remember to verify your project, else your uploads will be kept private.__",
+
+        "**Now lets authorise.**\n\nYou need to give me the access to upload videos to your Youtube account.For that open the given link and allow access and copy the code. Come back here and type /authorise copied-code and send it.\n\n**Fear not!**\nI'm not a hacker or someone who wants to creep into people's privacy. I respect one's privacy. I'm here just to help anyone who wants help. If I was a hacker I won't be sitting here writing Telegram Bots."
+    ]
+
+    NOT_A_REPLY_MSG = "رجاء قم بالرد على بعض الفيديوهات"
+
+    NOT_A_MEDIA_MSG = " لا يوجد فيديوهات"+NOT_A_REPLY_MSG
+
+    NOT_A_VALID_MEDIA_MSG = "هذا ليس ميديا"
+    
+    DAILY_QOUTA_REACHED = "يبدو أنك تحاول تحميل أكثر من 6 مقاطع فيديو اليوم! بشكل افتراضي ، يسمح موقع youtube بحوالي 6 تحميلات يوميًا فقط ، لذلك قد يفشل هذا الطلب !!"
+
+    PROCESSING = "معالجة طلبك....."
+
+    NOT_AUTHENTICATED_MSG = "لم تصدقني على تحميل الفيديو على أي حساب. انظر / ساعد في المصادقة"
+
+    NO_AUTH_CODE_MSG = "لا يوجد رمز. الرجاء تقديم بعض التعليمات البرمجية"
+
+    AUTH_SUCCESS_MSG = "قووووة رفع مميز"
+
+    AUTH_FAILED_MSG = "المصادقة فشلت\nDetails:{}"
+    
+    AUTH_DATA_SAVE_SUCCESS = "تم حفظ بيانات المصادقة المقدمة بنجاح!"
+    
